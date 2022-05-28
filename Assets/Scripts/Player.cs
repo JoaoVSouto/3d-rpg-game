a597d423a637df7eec0c75fd9a393110532ae15a
João Vítor <joaovsoutosantos@gmail.com>
2022-05-28 14:53:47 -0300

    feat: program player animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,23 @@ public class Player : MonoBehaviour
     public float smoothRotationTime;
 
     private float smoothTurnVelocity;
+    private Animator animator;
     private Transform cam;
     private CharacterController characterController;
     private Vector3 moveDirection;
 
+    private const int LEFT_CLICK_BUTTON_INDEX = 0;
+
+    private enum AnimationStates
+    {
+        Idle,
+        Walk,
+        Attack,
+    }
+
     void Start()
     {
+        animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
         cam = Camera.main.transform;
     }
@@ -22,6 +33,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+        GetMouseInput();
+    }
+
+    void SetTransition(AnimationStates state)
+    {
+        animator.SetInteger("transition", (int)state);
     }
 
     void Move()
@@ -46,14 +63,25 @@ public class Player : MonoBehaviour
                 transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
                 // mount the move direction forwared based on angle
                 moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
+
+                SetTransition(AnimationStates.Walk);
             } else
             {
                 moveDirection = Vector3.zero;
+                SetTransition(AnimationStates.Idle);
             }
         }
 
         moveDirection.y -= gravity * Time.deltaTime;
         // move the player itself
         characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void GetMouseInput()
+    {
+        if (characterController.isGrounded && Input.GetMouseButtonDown(LEFT_CLICK_BUTTON_INDEX))
+        {
+            SetTransition(AnimationStates.Attack);
+        }
     }
 }
