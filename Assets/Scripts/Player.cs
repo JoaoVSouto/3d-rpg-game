a076a72e00a82bf20a48e15f66e7f9c529f94e40
João Vítor <joaovsoutosantos@gmail.com>
2022-05-28 14:17:49 -0300

    fix: some player bugs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,40 +5,55 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float gravity;
     public float smoothRotationTime;
 
     private float smoothTurnVelocity;
-    private Transform camera;
+    private Transform cam;
     private CharacterController characterController;
+    private Vector3 moveDirection;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        camera = Camera.main.transform;
+        cam = Camera.main.transform;
     }
 
     void Update()
     {
-        // get horizontal input (right/left)
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        // get vertical input (up/down)
-        float vertical = Input.GetAxisRaw("Vertical");
-        
-        // vector pointing to horizontal and vertical direction result
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        Move();
+    }
 
-        if (direction.magnitude > 0)
+    void Move()
+    {
+        if (characterController.isGrounded)
         {
-            // get player and camera angles
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            // a smoothy angle derivative
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref smoothTurnVelocity, smoothRotationTime);
-            // rotate the user based on smooth angle
-            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-            // mount the move direction forwared based on angle
-            Vector3 moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
-            // move the player itself
-            characterController.Move(moveDirection * speed * Time.deltaTime);
+            // get horizontal input (right/left)
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            // get vertical input (up/down)
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            // vector pointing to horizontal and vertical direction result
+            Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+            if (direction.magnitude > 0)
+            {
+                // get player and camera angles
+                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                // a smoothy angle derivative
+                float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref smoothTurnVelocity, smoothRotationTime);
+                // rotate the user based on smooth angle
+                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+                // mount the move direction forwared based on angle
+                moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
+            } else
+            {
+                moveDirection = Vector3.zero;
+            }
         }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        // move the player itself
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
